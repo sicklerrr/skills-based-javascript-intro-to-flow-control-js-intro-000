0342cb4fbe3c83fc2115aad564c1220ec1478af0
Aaron Sickler <sicklera517@gmail.com>
2019-08-21 03:26:09 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,13 @@
-function basicTeenager(age){
-    if (age >= 13 && age <= 19){
-      return "You are a teenager!";
-  }
+function basicTeenager(age) {
+
 }
 
 function teenager(age) {
-    if (age >= 13 && age <= 19){
-      return "You are a teenager!";
-  }
-  else {
-    return "You are not a teenager";
-  }
+
 }
 
-function ageChecker(age)
+function ageChecker(age) {
+
 }
 
 function ternaryTeenager(age) {
