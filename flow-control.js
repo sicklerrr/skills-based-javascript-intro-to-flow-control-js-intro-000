0b1800abe8b699dcabb6e61d4fbd826ef33f3a61
Aaron Sickler <sicklera517@gmail.com>
2019-08-21 03:32:25 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -17,10 +17,10 @@ function ageChecker(age) {
   if (age >= 13 && age <= 19){
     return "You are a teenager!";
   }
-  else if (age < 13)
+  else if (age < 13){
     return "You are a kid";
   }
-  else if (age > 19)
+  else if (age > 19){
     return "You are a grownup";
 }
 
